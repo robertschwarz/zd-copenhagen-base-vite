3d1d35123dd12b4d451fdcef3fbf08a83e21b809
Robert Schwarz <robert.schwarz@personio.de>
2025-04-18 12:50:32 +0200

    update cjs vite config to run in library mode

--- vite.cjs.config.ts
+++ vite.cjs.config.ts
@@ -3,11 +3,22 @@
  * Resolves to src/theme/script.js and src/theme/style.css
  */
 import { defineConfig, loadEnv } from "vite";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/main.ts"),
+        name: "main",
+        fileName: "index.js",
+      },
+    },
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
